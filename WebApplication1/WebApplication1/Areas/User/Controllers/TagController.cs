b8947bb7f178c2ebd64f76eb8ec40e2cba04dcd0
Артём Шашок <artyom.shashok@mail.ru>
2024-03-31 15:04:37 +0300

    Added autofill for tag input field

---
+++ WebApplication1/WebApplication1/Areas/User/Controllers/TagController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.DataAccess.Repository.IRepository;
+
+namespace WebApplication1.Areas.User.Controllers
+{
+    [Area("User")]
+    public class TagController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public TagController(IUnitOfWork unitOfWork) 
+        { 
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> GetAllTags(string prefix)
+        {
+            var tags = await _unitOfWork.Tag.GetAllAsync(u => u.Name.StartsWith(prefix));
+            return Json(tags);
+        }
+    }
+}
